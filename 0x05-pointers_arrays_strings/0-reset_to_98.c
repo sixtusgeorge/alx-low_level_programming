bdcf7502e9a2183b10ef206236b1875b04cf1149
sixtusgeorge <lotusg07@gmail.com>
2022-09-20 04:55:23 -0700

    arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes the pointer and updates the valu
+ * @n: int to check
+ *  Owned By Lota
+ *   Return: 0 is success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
