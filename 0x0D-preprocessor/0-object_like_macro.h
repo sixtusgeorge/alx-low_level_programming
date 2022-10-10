a7c026e84288e503db56de6383aa7d255eaf7705
sixtusgeorge <lotusg07@gmail.com>
2022-10-10 12:47:10 -0700

    preprocessor

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
+#ifndef _0_OBJECT_LIKE_MARCO_H_
+#define _0_OBJECT_LIKE_MARCO_H_
 
-#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+#define SIZE 1024
 
-#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
+#endif
